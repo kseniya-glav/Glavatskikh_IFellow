39cb454c57acf093af12ef895a60a4b691112284
Ksesha <glavatskikh.kseni@gmail.com>
2024-09-08 15:06:42 +0500

    init commit

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,3 +1,28 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class RunTest {
-    //its RunTest origin
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("beforeAll_develop");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("afterAll_develop");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("1_develop");
+        Assertions.assertEquals("Samsung","Samsung");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("2_develop");
+        Assertions.assertEquals("Iphone","Samsung");
+    }
 }
